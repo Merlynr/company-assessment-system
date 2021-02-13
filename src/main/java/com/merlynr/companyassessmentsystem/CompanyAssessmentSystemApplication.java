86896da26a4096212bc35c3323f87a3b89c9f614
merlynr <lcq1013962426@gmail.com>
2021-02-13 18:32:53 +0800

    init

---
+++ src/main/java/com/merlynr/companyassessmentsystem/CompanyAssessmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.merlynr.companyassessmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyAssessmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyAssessmentSystemApplication.class, args);
+    }
+
+}
